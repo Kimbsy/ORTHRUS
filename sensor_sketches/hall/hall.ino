40d24fd415be8e096b0ec74bd3531285d0d4bedd
Kimbsy <lordkimber@gmail.com>
2015-08-02 13:52:42 +0100

    writing arduino sketches for sensors

---
+++ sensor_sketches/hall/hall.ino
@@ -0,0 +1,20 @@
+/**
+ * Sketch to measure rotation of hamster wheel with magnets attached.
+ *
+ * Using hall sensor (U18).
+ *
+ * Pins when looking at model number on sensor:
+ *   left   : 5v
+ *   middle : ground
+ *   right  : output
+ */
+
+public void setup()
+{
+
+}
+
+public void loop()
+{
+	
+}
